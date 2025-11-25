b21b3dc6868fe95b1f87d77b8ad4658b4b5ba037
AmineKhamlich <sk8.amine@gmail.com>
2025-11-25 19:22:14 +0100

    Creació Columnes ToDo, Doing, Done

--- Kanban/Kanban/MainWindow.xaml.cs
+++ Kanban/Kanban/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Kanban
         {
             InitializeComponent();
         }
+
+        private void Button_Click_AfegirTasca(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
